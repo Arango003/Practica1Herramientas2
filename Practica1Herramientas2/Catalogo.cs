955c839b33970a2aa2d7e9708b2e10e267f9e8c2
Arango003 <Usuario@DESKTOP-TD49UJL>
2022-09-14 11:13:00 -0500

    Agregar archivos de proyecto.

---
+++ Practica1Herramientas2/Catalogo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1Herramientas2
+{
+    internal class Catalogo
+    {
+        List<Pelicula> peliculas = new List<Pelicula>();
+
+
+        public int ValidarPelicula(string nombrePelicula)
+        {
+            int index = this.peliculas.FindIndex(pelicula => pelicula.Titulo == nombrePelicula);
+
+            return index;
+        }
+
+        public void AgregarPelicula()
+        {
+            int año, posicion;
+            string titulo, idioma, productora, director, genero, duracion;
+
+            Console.WriteLine("Ingrese el nombre de la pelicula");
+            titulo = Console.ReadLine();
+            posicion = ValidarPelicula(titulo);
+            if (posicion == -1)
+            {
+                Console.WriteLine("Ingrese el idioma");
+                idioma = Console.ReadLine();
+                Console.WriteLine("Ingrese el nombre de la productora");
+                productora = Console.ReadLine();
+                Console.WriteLine("Ingrese el nombre del director");
+                director = Console.ReadLine();
+                Console.WriteLine("Ingrese el genero de la pelicula");
+                genero = Console.ReadLine();
+                Console.WriteLine("Ingrese la duracion de la pelicula");
+                duracion = Console.ReadLine();
+                Console.WriteLine("Ingrese el año de lanzamiento de la pelicula");
+                año = int.Parse(Console.ReadLine());
+
+                this.peliculas.Add(new Pelicula()
+                {
+                    Titulo = titulo,
+                    Idioma = idioma,
+                    Productora = productora,
+                    Director = director,
+                    Genero = genero,
+                    Duracion = duracion
+                });
+            } else
+            {
+                Console.WriteLine("La pelicula ya existe");
+            }
+
+        }
+
+        public void ActualizarPelicula()
+        {
+            int año, posicion;
+            string titulo, idioma, productora, director, genero, duracion;
+
+            Console.WriteLine("Ingrese el nombre de la pelicula");
+            titulo = Console.ReadLine();
+            posicion = ValidarPelicula(titulo);
+
+            if (posicion == -1)
+            {
+                Console.WriteLine("Esta pelicula no existe en el catalogo");
+            } else
+            {
+                this.peliculas[posicion] = new Pelicula()
+                {
+                    Titulo=titulo,
+
+                }
+            }
+        }
+    }
+}
---
+++ Practica1Herramientas2/Pelicula.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1Herramientas2
+{
+    internal class Pelicula
+    {
+        int año;
+        string titulo, idioma, productora, director, genero, duracion;
+
+        public Pelicula() { }
+        public int Año { get => año; set => año = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Duracion { get => duracion; set => duracion = value; }
+        public string Idioma { get => idioma; set => idioma = value; }
+        public string Productora { get => productora; set => productora = value; }
+        public string Director { get => director; set => director = value; }
+        public string Genero { get => genero; set => genero = value; }
+    }
+}
